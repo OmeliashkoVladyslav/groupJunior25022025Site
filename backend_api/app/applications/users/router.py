1a1a46ba096aec99b454560f3b6dba7884210ffa
OmeliashkoVladyslav <merven357@icloud.com>
2025-04-29 20:13:26 +0300

    userfields

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -3,8 +3,7 @@ from applications.users.schemas import RegisterUserFields, BaseFields
 
 router_users = APIRouter()
 
+
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(
-    new_user: RegisterUserFields
-) -> BaseFields:
+async def create_user(new_user: RegisterUserFields) -> BaseFields:
     return new_user
