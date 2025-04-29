79c363b49428231db1248490460068b10dae96b3
OmeliashkoVladyslav <merven357@icloud.com>
2025-04-29 19:38:52 +0300

    userfields

--- backend_api/app/applications/users/schemas.py
+++ backend_api/app/applications/users/schemas.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel, EmailStr, Field
 
-class UserFields(BaseModel):
-    email: EmailStr = Field(description="User_email", examples=['test_hillel_api_mailing@ukr.net'])
+class BaseFields(BaseModel):
+    email: EmailStr = Field(description="User email", examples=['test_hillel_api_mailing@ukr.net'])
+    name: str = Field(description='User nickname', examples=['Casper'])
+
+class PasswordField(BaseModel):
+    password: str = Field(min_length=8)
+
+
+class RegisterUserFields(BaseFields, PasswordField):
+    pass
