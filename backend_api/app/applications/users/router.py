793b395d73217a829c97556802117298023151bc
OmeliashkoVladyslav <merven357@icloud.com>
2025-05-09 20:26:53 +0300

    alembic auto

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from applications.users.models import User
 from applications.users.schemas import BaseFields, RegisterUserFields
 from database.session_dependencies import get_async_session
 
 router_users = APIRouter()
+
+async def create_user(email, name, password, session: AsyncSession):
+    new_user = User(email=email, name=name, hashed_password=password)
+    session.add(new_user)
+    await session.commit()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
